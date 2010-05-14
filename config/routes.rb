b44ffa351f9b5b69cc67562ea1631a8e1b379078
Andres Hernandez <Andres@Andres.local>
2010-05-14 11:51:52 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.signup  '/signup',  :controller => 'users', :action => 'new'
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
