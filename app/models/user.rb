ac269d53e1324b2435406612dfcb95db48d77bd7
Andres Hernandez <Andres@Andres.local>
2010-05-14 12:50:51 -0600

    Annotated models

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+# Schema version: 20100514184617
+#
+# Table name: users
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+end
