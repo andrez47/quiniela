56bb0b893c9dfa2eb0f0117e8c680e0b838585d6
Andres Hernandez <Andres@Andres.local>
2010-05-14 17:50:17 -0600

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,7 @@ module ApplicationHelper
     end
   end
 
+  def logo
+    image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,11 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
+  def remember_me!
+    self.remember_token = encrypt("#{salt}--#{id}")
+    save_without_validation
+  end
+
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
     return nil  if user.nil?
@@ -43,8 +48,10 @@ class User < ActiveRecord::Base
 private
 
   def encrypt_password
-    self.salt = make_salt
-    self.encrypted_password = encrypt(password)
+    unless password.nil?
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
   end
 
   def encrypt(string)
